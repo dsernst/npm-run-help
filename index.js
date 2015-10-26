0ef68ade7f6c087e2589234333d1f5e779485040
David Ernst <git@dsernst.com>
2015-10-25 21:37:18 -0700

    Improve formatting

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log('Available run-scripts:')
+console.log('Usage')
+console.log('  npm run [NAME]')
+console.log('')
+console.log('Available scripts:')
 console.log(require('./package.json').scripts)
+console.log()
